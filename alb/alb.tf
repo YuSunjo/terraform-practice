99fd00f5faf502b40789169ace418080399b4895
YuSunjo <tnswh2023@gmail.com>
2022-05-14 18:50:02 +0900

    feat(terraform): alb.tf 파일 생성 및 security group, subnet 적용

---
+++ alb/alb.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "vpc_id" {
+  default = "vpc-5ab22e31"
+}
+
+variable "subnet_id" {
+  default = ["subnet-4fc00f10", "subnet-c076848f"]
+}
+
+data "aws_vpc" "foo" {
+
+}
+
+data "aws_subnet_ids" "example" {
+  vpc_id = data.aws_vpc.foo.id
+}
+
+data "aws_subnet" "example" {
+  for_each = data.aws_subnet_ids.example.ids
+  id       = each.value
+}
+
+output "vpc_id" {
+  value = data.aws_vpc.foo.id
+}
+
+output "subnet_cidr_blocks" {
+  value = [for s in data.aws_subnet.example : s.cidr_block]
+}
+
+resource "aws_security_group" "allow_alb" {
+  name        = "allow_alb"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "alb from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_alb"
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_alb.id]
+#  subnets            = var.subnet_id
+  subnets            = data.aws_subnet_ids.example.ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "production"
+  }
+}
