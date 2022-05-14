1692bcdf065e5d93449238b8381f5e36fc3a9c8f
YuSunjo <tnswh2023@gmail.com>
2022-05-14 19:16:59 +0900

    feat(terraform): target attachment

--- alb/alb.tf
+++ alb/alb.tf
@@ -27,6 +27,10 @@ output "vpc_id" {
   value = data.aws_vpc.foo.id
 }
 
+output "alb_dns_name" {
+  value = "aws_lb.test.dns_name"
+}
+
 output "subnet_cidr_blocks" {
   value = [for s in data.aws_subnet.example : s.cidr_block]
 }
@@ -101,5 +105,37 @@ resource "aws_lb_listener" "front_end" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.test.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "test-2a" {
+  for_each = toset(data.aws_instances.test.private_ips)
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = each.value
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "test-2a" {
+  count = toset(data.aws_instances.test.private_ips)
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = element(data.aws_instances.test.private_ips, count.index)
+  port             = 80
+}
+
+#resource "aws_lb_target_group_attachment" "test-2a" {
+#  target_group_arn = aws_lb_target_group.test.arn
+#  target_id        = data.aws_instances.test.private_ips[0]
+#  port             = 80
+#}
+#
+#resource "aws_lb_target_group_attachment" "test-2c" {
+#  target_group_arn = aws_lb_target_group.test.arn
+#  target_id        = data.aws_instances.test.private_ips[1]
+#  port             = 80
+#}
+
+data "aws_instances" "test" {
+  instance_tags = {
+    Name = "web-*"
   }
 }
