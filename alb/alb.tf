0ec506e9457849fd2ee82e08c93ec82c8612d3e3
YuSunjo <tnswh2023@gmail.com>
2022-05-14 18:58:24 +0900

    feat(terraform): aws_lb_listener 구성

--- alb/alb.tf
+++ alb/alb.tf
@@ -91,5 +91,15 @@ resource "aws_lb_target_group" "test" {
     timeout = 2
     unhealthy_threshold = 2
   }
+}
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
 }
