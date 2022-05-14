ffbd5aadb452fae8715f56b7196bf91151e28a4a
YuSunjo <tnswh2023@gmail.com>
2022-05-14 18:07:27 +0900

    feat(terraform): security group 생성 및 적용

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+variable "vpc_id" {
+  default = "vpc-5ab22e31"
+}
+
 data "aws_ami" "amzn2" {
   most_recent = true
 
@@ -19,11 +23,38 @@ data "aws_ami" "amzn2" {
   owners = ["amazon"] # Canonical
 }
 
+resource "aws_security_group" "allow_web" {
+  name = "allow_web"
+  description = "allow web inbound traffic"
+  vpc_id = var.vpc_id // 위에 variable 참조
+
+  ingress {
+    description      = "web from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amzn2.id
   instance_type = "t2.micro"
   key_name = "tf-key-pair"
+  vpc_security_group_ids = [aws_security_group.allow_web.id] // 위에꺼 참조
 
   root_block_device {
     volume_size = 30
