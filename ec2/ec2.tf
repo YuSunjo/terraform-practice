a65793a49841385383b053c1dfba62ec0f3d43f7
YuSunjo <tnswh2023@gmail.com>
2022-05-14 16:37:18 +0900

    feat(terraform): key pair 적용 및 볼륨 늘리기

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -23,6 +23,11 @@ data "aws_ami" "amzn2" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amzn2.id
   instance_type = "t2.micro"
+  key_name = "tf-key-pair"
+
+  root_block_device {
+    volume_size = 30
+  }
 
   tags = {
     Name = "web"
