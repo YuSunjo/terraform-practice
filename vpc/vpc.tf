f8097ded6d6e9ba344a49c33daa48bbd6e9703f6
유순조 <tnswh2023@gmail.com>
2022-05-16 17:14:41 +0900

    feat(vpc): private subnet용 route table

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "subnet-pub2-10-0-2-0" {
   }
 }
 
-resource "aws_subnet" "subnet-pri2-10-0-3-0" {
+resource "aws_subnet" "subnet-pri1-10-0-3-0" {
   vpc_id     = aws_vpc.vpc_10-0-0-0.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "ap-northeast-2a"
@@ -83,4 +83,30 @@ resource "aws_route_table_association" "rt-pub-as1-vpc-10-0-0-0" {
 resource "aws_route_table_association" "rt-pub-as2-vpc-10-0-0-0" {
   subnet_id      = aws_subnet.subnet-pub2-10-0-2-0
   route_table_id = aws_route_table.rt-pub-vpc-10-0-0-0
+}
+
+resource "aws_route_table" "rt-pri1-vpc-10-0-0-0" {
+  vpc_id = aws_vpc.vpc_10-0-0-0.id
+
+  tags = {
+    Name = "rt-pri1-vpc-10-0-0-0"
+  }
+}
+
+resource "aws_route_table" "rt-pri2-vpc-10-0-0-0" {
+  vpc_id = aws_vpc.vpc_10-0-0-0.id
+
+  tags = {
+    Name = "rt-pri2-vpc-10-0-0-0"
+  }
+}
+
+resource "aws_route_table_association" "rt-pri1-as1-vpc-10-0-0-0" {
+  subnet_id      = aws_subnet.subnet-pri1-10-0-3-0
+  route_table_id = aws_route_table.rt-pri1-vpc-10-0-0-0
+}
+
+resource "aws_route_table_association" "rt-pri-as2-vpc-10-0-0-0" {
+  subnet_id      = aws_subnet.subnet-pri2-10-0-4-0
+  route_table_id = aws_route_table.rt-pri2-vpc-10-0-0-0
 }
